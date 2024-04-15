01ab0ee87ebd72d15d9bfbb50a55b783c744277d
davide-piarulli <davide.piarulli.1@gmail.com>
2024-04-15 15:46:01 +0100

    fix foreach

--- index.php
+++ index.php
@@ -46,28 +46,27 @@ $hotels = [
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   <title>Hotel</title>
 </head>
 
 <body>
   <div class="container my-5 ">
     <div class="row">
-      <ul>
-        <?php
-        foreach ($hotels as $hotel) : {
-            // echo $hotel('name');
-          }
-        ?>
-          <li><?php echo $hotel['name'] ?></li>
-          <ul>
-            <li><?php echo $hotel['description'] ?></li>
-            <li>Parcheggio: <?php echo $hotel['parking'] ? 'Si' : 'No' ?></li>
-            <li>Voto: <?php echo $hotel['vote'] ?></li>
-            <li>KM dal centro: <?php echo $hotel['distance_to_center'] ?></li>
-          </ul>
-
-        <?php endforeach ?>
-      </ul>
+      <?php foreach ($hotels as $hotel) : ?>
+        <div class="col d-flex flex-wrap my-2 ">
+          <div class="card" style="width: 18rem;">
+            <h2><?php echo $hotel['name'] ?></h2>
+            <ul>
+              <li><?php echo $hotel['description'] ?></li>
+              <li>Parcheggio: <?php echo $hotel['parking'] ? 'Si' : 'No' ?></li>
+              <li>Voto: <?php echo $hotel['vote'] ?></li>
+              <li>Distanza dal centro: <?php echo $hotel['distance_to_center'] ?> Km</li>
+            </ul>
+          </div>
+        </div>
+      <?php endforeach ?>
     </div>
   </div>
 </body>
